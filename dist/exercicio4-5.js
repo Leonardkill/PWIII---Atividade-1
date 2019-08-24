7595dabba6129d769a9ea632455cd07183d7281c
Leonardo <leonardofaria_7@hotmail.com>
2019-08-23 21:35:55 -0300

    Exercicio 4-5 - Par ou Impar

---
+++ dist/exercicio4-5.js
@@ -0,0 +1,8 @@
+var numero = 100;
+var resto = numero % 2;
+if (resto == 0) {
+    console.log("par");
+}
+else {
+    console.log("impar");
+}
---
+++ exercicio4-5.ts
@@ -0,0 +1,9 @@
+let numero = 100;
+let resto = numero % 2;
+
+if(resto == 0){
+    console.log("par")
+}
+else{
+    console.log("impar")
+}
